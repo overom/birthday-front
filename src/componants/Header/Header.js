0efd77c65fc072a9476b3cdb87ac0b3bb879bc0a
Squaloshiro <mehdi.link@hotmail.fr>
2021-08-07 13:33:35 +0200

    quest rep

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -10,13 +10,12 @@ const Header = ({ isLoggedin, setIsLoggedin }) => {
     <div>
       <div className="header-title">
         <div>Bon Anniversaire Audrey</div>
+        <br />
+        <div>Seras-tu le premier à trouver la clé qui mene au coffre caché ?</div>
       </div>
       {!isLoggedin && (
         <div className="lmj-banner flex">
-          <Button
-            onClick={() => history.push("/inscription")}
-            title="Inscription"
-          />
+          <Button onClick={() => history.push("/inscription")} title="Inscription" />
         </div>
       )}
     </div>
